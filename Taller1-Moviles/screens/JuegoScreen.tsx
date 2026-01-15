ce9a5145a835c4e3d4f601604fd6b412140b6d4c
Erick Chango <lara41183@gmail.com>
2026-01-15 21:53:34 +0100

    integracion de persistencia de datos con Supabase para el ranking

--- Taller1-Moviles/screens/JuegoScreen.tsx
+++ Taller1-Moviles/screens/JuegoScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { supabase } from '../superbase/SuperbaseConfig';
 
 const { width, height } = Dimensions.get('window');
 
@@ -8,17 +9,20 @@ export default function JuegoScreen({ route }: any) {
   const [score, setScore] = useState(0);
   const [pos, setPos] = useState({ t: 200, l: 150 });
 
-  const moveMosquito = () => {
-    setScore(score + 1);
+  const moveMosquito = async () => {
+    const newScore = score + 1;
+    setScore(newScore);
     setPos({
       t: Math.random() * (height - 300) + 50,
       l: Math.random() * (width - 100) + 20
     });
+ 
+    await supabase.from('ranking').upsert([{ usuario: username, puntos: newScore }]);
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.scoreBoard}>Puntos: {score}</Text>
+      <Text style={styles.scoreBoard}>Cazador: {username} | Puntos: {score}</Text>
       <TouchableOpacity style={[styles.mosquito, { top: pos.t, left: pos.l }]} onPress={moveMosquito}>
         <Text style={{ fontSize: 60 }}>🦟</Text>
       </TouchableOpacity>
@@ -27,7 +31,7 @@ export default function JuegoScreen({ route }: any) {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#fff' },
-  scoreBoard: { fontSize: 24, textAlign: 'center', marginTop: 40 },
+  container: { flex: 1, backgroundColor: '#f1f2f6' },
+  scoreBoard: { fontSize: 20, textAlign: 'center', marginTop: 20, fontWeight: 'bold' },
   mosquito: { position: 'absolute' }
 });

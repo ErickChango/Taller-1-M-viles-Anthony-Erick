0e81e5bf238ca4f17fd65113334fbb183e7330d5
Erick Chango <lara41183@gmail.com>
2026-01-15 21:35:41 +0100

    optimizacioon de  manejo de parametros y mejora de tipado

--- Taller1-Moviles/screens/WelcomeScreen.tsx
+++ Taller1-Moviles/screens/WelcomeScreen.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function WelcomeScreen({ route, navigation }: any) {
-  const { username } = route.params;
+
+interface WelcomeProps {
+  route: { params?: { username?: string } };
+  navigation: any;
+}
+
+export default function WelcomeScreen({ route, navigation }: WelcomeProps) {
+
+  const username = route?.params?.username || 'Invitado';
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>¡Bienvenido,</Text>
       <Text style={styles.user}>{username}!</Text>
-      <TouchableOpacity style={styles.btn} onPress={() => navigation.replace('Main', { username })}>
+      
+      <TouchableOpacity 
+        style={styles.btn} 
+        onPress={() => navigation.replace('Main', { username })}
+      >
         <Text style={styles.btnText}>EMPEZAR PARTIDA</Text>
       </TouchableOpacity>
     </View>
@@ -15,9 +27,38 @@ export default function WelcomeScreen({ route, navigation }: any) {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#2ecc71' },
-  title: { fontSize: 24, color: '#fff' },
-  user: { fontSize: 45, fontWeight: 'bold', color: '#fff', marginBottom: 40 },
-  btn: { backgroundColor: '#fff', paddingHorizontal: 40, paddingVertical: 15, borderRadius: 30 },
-  btnText: { color: '#2ecc71', fontWeight: 'bold' }
+  container: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: '#2ecc71' 
+  },
+  title: { 
+    fontSize: 24, 
+    color: '#fff' 
+  },
+  user: { 
+    fontSize: 45, 
+    fontWeight: 'bold', 
+    color: '#fff', 
+    marginBottom: 40,
+    textAlign: 'center'
+  },
+  btn: { 
+    backgroundColor: '#fff', 
+    paddingHorizontal: 40, 
+    paddingVertical: 15, 
+    borderRadius: 30,
+   
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+  },
+  btnText: { 
+    color: '#2ecc71', 
+    fontWeight: 'bold',
+    fontSize: 16
+  }
 });

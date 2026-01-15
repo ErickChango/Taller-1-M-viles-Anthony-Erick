82ca85271b20c4fcc50c15e6d0c5e83357373ed9
Erick Chango <lara41183@gmail.com>
2026-01-15 21:10:44 +0100

    implementacion de la lógica básica y navegación en WelcomeScreen

--- Taller1-Moviles/screens/WelcomeScren.tsx
+++ Taller1-Moviles/screens/WelcomeScren.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 export default function WelcomeScreen({ route, navigation }: any) {
   const { username } = route.params;

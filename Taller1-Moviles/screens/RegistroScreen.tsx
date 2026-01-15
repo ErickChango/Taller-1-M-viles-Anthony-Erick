85fed7ba245a2842cf394c5e9fed50b02c26f5a0
anthony6514 <anthonyruiz20048@gmail.com>
2026-01-15 18:43:27 -0500

    Anthony: Correccion ortografica y aplicacional Login

--- Taller1-Moviles/screens/RegistroScreen.tsx
+++ Taller1-Moviles/screens/RegistroScreen.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { supabase } from '../superbase/SuperbaseConfig';
 
-export default function RegistroScreen() {
+export default function RegistroScreen({ navigation }: any) {
+  const [user, setUser] = useState('');
+
+  const registrarGuerrero = async () => {
+    if(!user) return Alert.alert("Error", "El alias es obligatorio");
+    const { error } = await supabase.from('usuarios').insert([{ username: user }]);
+    if (!error) {
+      Alert.alert("EXITO", "Guerrero registrado en la base de datos");
+      navigation.navigate('Login');
+    }
+  };
+
   return (
-    <View>
-      <Text>Pagina para Registrarce</Text>
-      <TextInput placeholder="Tu Usuariio" />
-      <TouchableOpacity>
-        <Text>GUARDAR</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>NUEVO REGISTRO</Text>
+      <TextInput style={styles.input} placeholder="Alias" placeholderTextColor="#555" onChangeText={setUser} />
+      <TouchableOpacity style={styles.btn} onPress={registrarGuerrero}>
+        <Text style={styles.btnText}>GUARDAR DATOS</Text>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#000', justifyContent: 'center', alignItems: 'center' },
+  title: { fontSize: 24, color: '#00fbff', marginBottom: 30 },
+  input: { width: '85%', padding: 15, backgroundColor: '#111', color: '#fff', borderRadius: 5, borderBottomWidth: 2, borderBottomColor: '#00fbff' },
+  btn: { backgroundColor: '#00fbff', padding: 15, width: '85%', marginTop: 30 },
+  btnText: { color: '#000', textAlign: 'center', fontWeight: 'bold' }
+});

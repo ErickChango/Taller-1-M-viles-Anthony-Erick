3cf114f4c8a9a161b2e08963b7eff10a8212f60a
ErickSam <erickdaxe@gmail.com>
2026-01-21 19:26:54 -0500

    diseñar interfaz de login con campos de email y password

--- screens/loginScreen.tsx
+++ screens/loginScreen.tsx
@@ -2,19 +2,39 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function LoginScreen({ navigation }: any) {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const manejarLoginProvisional = () => {
+
+    navigation.navigate('Welcome', { username: 'Jugador Prueba' });
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>CYBER HUNTER</Text>
+      <Text style={styles.title}>Juegos Login</Text>
+      
       <TextInput 
         style={styles.input} 
-        placeholder="Alias del Guerrero" 
+        placeholder="Correo Electrónico" 
         placeholderTextColor="#555"
-        onChangeText={setUsername} 
+        autoCapitalize="none"
+        onChangeText={setEmail} 
       />
-      <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Welcome', { username })}>
-        <Text style={styles.btnText}>INICIAR MISION</Text>
+      <TextInput 
+        style={styles.input} 
+        placeholder="Contraseña" 
+        placeholderTextColor="#555"
+        secureTextEntry
+        onChangeText={setPassword} 
+      />
+
+      <TouchableOpacity style={styles.btn} onPress={manejarLoginProvisional}>
+        <Text style={styles.btnText}>INICIAR SESION</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate('Registro')} style={{ marginTop: 20 }}>
+        <Text style={{ color: '#00fbff', textDecorationLine: 'underline' }}>¿No tienes cuenta? Regístrate aquí</Text>
       </TouchableOpacity>
     </View>
   );
@@ -23,7 +43,7 @@ export default function LoginScreen({ navigation }: any) {
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: '#000', justifyContent: 'center', alignItems: 'center' },
   title: { fontSize: 32, color: '#00fbff', fontWeight: 'bold', marginBottom: 40, letterSpacing: 3 },
-  input: { width: '85%', padding: 15, backgroundColor: '#111', color: '#fff', borderRadius: 5, borderWidth: 1, borderColor: '#00fbff' },
-  btn: { backgroundColor: '#00fbff', padding: 15, borderRadius: 5, width: '85%', marginTop: 20 },
+  input: { width: '85%', padding: 15, backgroundColor: '#111', color: '#fff', borderRadius: 5, borderWidth: 1, borderColor: '#00fbff', marginBottom: 15 },
+  btn: { backgroundColor: '#00fbff', padding: 15, borderRadius: 5, width: '85%', marginTop: 10 },
   btnText: { color: '#000', textAlign: 'center', fontWeight: 'bold', fontSize: 16 }
 });
